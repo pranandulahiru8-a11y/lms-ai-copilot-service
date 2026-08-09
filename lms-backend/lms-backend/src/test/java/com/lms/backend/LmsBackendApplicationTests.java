c235e8d2cb2b28323265eec48bfd780638684d27
Manoj wickramasingha <141001887+ManojWickramasingha@users.noreply.github.com>
2026-08-09 13:22:08 +0530

    ci: setup jacoco code coverage and application-test.yml [QA-01]

--- lms-backend/lms-backend/src/test/java/com/lms/backend/LmsBackendApplicationTests.java
+++ lms-backend/lms-backend/src/test/java/com/lms/backend/LmsBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.lms.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LmsBackendApplicationTests {
 
 	@Test
